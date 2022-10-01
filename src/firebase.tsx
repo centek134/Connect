a52a0c33ca029ad10a5b30aef6ef312891377bf5
centek134 <centek134@wp.pl>
2022-10-01 21:00:59 +0200

    changes for now I've removed firebase config, there is a lot of confidential data, I'm adding it locally during app development. I need to find a way to hide it from people xd

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,1 +1,8 @@
-export {};
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection, getDocs} from "firebase/firestore/lite"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export {app, db, collection, getDocs,};
