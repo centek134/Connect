37e76593ea031683bd96749c8e1669bec67321e6
centek134 <centek134@wp.pl>
2022-10-01 21:00:35 +0200

    setting up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./assets/styles/App/App.css";
-import {Chat, Sidebar, Header} from "./components/index"; 
+import {Chat, Sidebar, Header} from "./components/index";
+import {Routes, Route } from "react-router-dom";
 const App = () => {
+  const test = <div>TESTTTTTTTTTTTTTTTTTTT</div>;
   return (
     <div className="app">
-        <Header/>
       <section className="app_body">
+        <Header/>
         <Sidebar/>
-        <Chat/>
+        <Routes>
+          <Route path="/" element={<Chat/>}/> 
+          <Route path="/room/:roomid" element={test}/>
+        </Routes>
       </section>
     </div>
   );
