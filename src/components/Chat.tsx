38eddad0b6ee1d23e6dc8179589d9b8e5f55bc20
centek134 <centek134@wp.pl>
2022-10-01 21:08:38 +0200

    setting and testing routes

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import "../assets/styles/Chat/Chat.css";
 export const Chat = () => {
   return (
-    <section className="chat_body">Chat</section>
+    <main className="chat_container">
+      <header className="chat_header">
+        <h3 className="chat_header_name">#Channel name</h3>
+      </header>
+      <section className="chat_body"></section>
+      <section className="chat_panel">
+      </section>
+    </main>
   )
 }
