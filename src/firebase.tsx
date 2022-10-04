8405ca006ebcaff40cb2f40ae928f1203ef2edbc
centek134 <centek134@wp.pl>
2022-10-04 17:43:04 +0200

    changes switched import routes, we are now importing firebase instances from firebase, not firebase/lite

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,8 +1,17 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore, collection, getDocs} from "firebase/firestore/lite"
+import {getFirestore, collection, getDocs} from "firebase/firestore"
+import {doc} from "firebase/firestore"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAZNYL0Bgr9Yali5XVP1ZbdzSo99dHB8_U",
+  authDomain: "contact-6b012.firebaseapp.com",
+  projectId: "contact-6b012",
+  storageBucket: "contact-6b012.appspot.com",
+  messagingSenderId: "959799982343",
+  appId: "1:959799982343:web:11b2945cf432822f1aada2",
+  measurementId: "G-C3BKWBJ012"
+};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
-export {app, db, collection, getDocs,};
+export { db, app, collection, getDocs, doc};
