d87424e51c7b18444d1e592c89e64094d13703fc
centek134 <centek134@wp.pl>
2022-10-04 17:41:49 +0200

    changes removed route with "/" removed test div

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import "./assets/styles/App/App.css";
 import {Chat, Sidebar, Header} from "./components/index";
 import {Routes, Route } from "react-router-dom";
 const App = () => {
-  const test = <div>TESTTTTTTTTTTTTTTTTTTT</div>;
   return (
     <div className="app">
       <section className="app_body">
         <Header/>
         <Sidebar/>
         <Routes>
-          <Route path="/" element={<Chat/>}/> 
-          <Route path="/room/:roomid" element={test}/>
+          <Route path="/room/:roomid" element={<Chat/>}/>
         </Routes>
       </section>
     </div>
