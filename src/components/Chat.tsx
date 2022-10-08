7af795ff99f8354559f50674ec4b11d559019ec7
centek134 <centek134@wp.pl>
2022-10-08 12:53:30 +0200

    new function Added checkKeyCode function, its checking if the user press "enter" and if that's so we are sending message and clearing text area timestamp name changes to timeStamp imported message component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,15 +1,15 @@
-import React, {useState, useEffect, ChangeEvent} from "react";
-import { db, collection ,getDocs, doc, addDoc} from "../firebase";
+import React, {useState, useEffect} from "react";
+import "../assets/styles/Chat/Chat.css";
 import {ReactComponent as Arrow } from "../assets/icons/Arrow.svg";
 import {ReactComponent as AddFileIcon } from "../assets/icons/AddFileIcon.svg";
+import { Message } from "./index";
+import { db, collection ,getDocs, doc, addDoc, getDoc, serverTimestamp} from "../firebase";
 import {useParams} from "react-router-dom";
-import { getDoc, serverTimestamp } from "firebase/firestore";
-import "../assets/styles/Chat/Chat.css";
 
 export const Chat = () => {
   useParams();
   let roomId = window.location.pathname.slice(6);
-  const [messages, setMessages] = useState<{message:string, userName:string, userImage:string, timestamp:number}[]>([]);
+  const [messages, setMessages] = useState<{message:string, userName:string, userImage:string, timeStamp:string}[]>([]);
   const [messageInput, setMessageInput] = useState<string> ("");
   const [roomName, setRoomName] = useState<string>("")
   
@@ -17,28 +17,37 @@ export const Chat = () => {
     fetchServerData();
   },[roomId]);
   
-
   async function fetchServerData(){
     const querySnapshotMessages = await getDocs(collection(db,`rooms/${roomId}/messages`));
     setMessages(querySnapshotMessages.docs.map((doc) => ({
       message:doc.data().message,
       userName:doc.data().userName,
       userImage:doc.data().userImage,
-      timestamp:doc.data().timestamp.seconds,
-
-    })))
+      timeStamp:doc.data().timeStamp,
+    })));
+    console.log(messages);
     const roomRef = doc(db, `rooms/${roomId}`);
     const roomSnap = await getDoc(roomRef);
     setRoomName(roomSnap.data()!.name)
   }
-  const writeMessage = (e:ChangeEvent<HTMLTextAreaElement>) => {
+  const writeMessage = (e:React.ChangeEvent<HTMLTextAreaElement>) => {
     setMessageInput(e.target.value);
+  }
+
+  const checkKeyCode = (e:React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if(e.key === "Enter"){
+      sendMessage();
+      setMessageInput("");
+    }
+    else{
+      return false;
+    }
   }
 
   async function sendMessage(){
     const docRef = await addDoc(collection(db,`rooms/${roomId}/messages`),{
       message:messageInput,
-      timestamp:serverTimestamp(),
+      timeStamp:serverTimestamp(),
       userImage:"",
       userName:""
     })
@@ -55,11 +64,13 @@ export const Chat = () => {
         <h3 className="chat_header_name">{roomName}</h3>
       </header>
       <section className="chat_body">
-        {messages.map((item,i) => <p key={i}>{item.message}</p>)}
+        {messages.map((item,i) => {
+          return <Message key={i} message={item.message} timeStamp={item.timeStamp} userImage={item.userImage} userName={item.userName}/>
+        })}
       </section>
       <section className="chat_panel">
         <div className="chat_container">
-          <textarea onChange={(e) => writeMessage(e)} placeholder="Jot something down..." className="chat_textarea">
+          <textarea onKeyDown={(e) => checkKeyCode(e)} onChange={(e) => writeMessage(e)} value={messageInput} placeholder="Jot something down..." className="chat_textarea">
           </textarea>
           <div className="control_panel">
             <button className="add_file_btn"><AddFileIcon/></button>
