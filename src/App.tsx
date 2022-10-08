36182411c3196d3027914ba72f8d85c67fea2227
centek134 <centek134@wp.pl>
2022-10-08 12:50:41 +0200

    testing login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./assets/styles/App/App.css";
-import {Chat, Sidebar, Header, WelcomePage} from "./components/index";
+import {Chat, Sidebar, Header, WelcomePage, Login} from "./components/index";
 import {Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
-      <section className="app_body">
-        <Header/>
-        <Sidebar/>
-        <Routes>
-        <Route path="/" element={<WelcomePage/>}/>
-          <Route path="/room/:roomid" element={<Chat/>}/>
-        </Routes>
-      </section>
+      <Login/>
     </div>
   );
-}
-
+};
+      // <section className="app_body">
+      //   <Header/>
+      //   <Sidebar/>
+      //   <Routes>
+      //   <Route path="/" element={<WelcomePage/>}/>
+      //     <Route path="/room/:roomid" element={<Chat/>}/>
+      //   </Routes>
+      // </section>
 export default App;
