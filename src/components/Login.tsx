60099f7bbd7857126b8bbf22a7d1a6a597c1da33
centek134 <centek134@wp.pl>
2022-10-15 15:46:16 +0200

    changes added functionality for guest login, so they can also enjoy chat

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,37 +1,45 @@
 import React, { Dispatch, SetStateAction } from "react";
 import "../assets/styles/Login/Login.css";
+import Avatar from "../assets/icons/Avatar.svg"
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
-import {  } from "firebase/app"
 import  "firebase/compat/app";
 
 interface Props{
-    setUser: Dispatch<SetStateAction<{}>>;
+    setUser: Dispatch<SetStateAction<null | {name:string,userImg:string}>>;
 }
 
 export const Login = ({setUser}:Props) => {
     
 
-    const signIn = () => {
+    const signInGoogle = () => {
         const auth = getAuth();
         const provider = new GoogleAuthProvider();
         signInWithPopup(auth,provider).then(result => {
             setUser({
-                name:result.user.displayName,
-                userImg:result.user.photoURL
+                name:result.user.displayName!,
+                userImg:result.user.photoURL!
                 })
             console.log(result);
-        }).catch((err) => console.log(err))
-    
+        }).catch((err) => console.log(err));
     };
 
+    const signInGuest = () => {
+        const randomNumber: number = Math.floor(Math.random()*10000) + 1;
+        const name = `Guest${randomNumber.toString()}`;
+        setUser({
+            name:name,
+            userImg:Avatar
+        });
+        console.log(name);
+    };
   return (
     <section className="login_section">
         <div className="login_panel">
             <h1 className="panel_header">Contact</h1>
             <p className="panel_text">Do you like typing with people? Don't let this opportunity slip away! Let's join us and start making friends.</p>
             <div className="button_container">
-            <button className="login_btn google" onClick={signIn}>Sign in with Google</button>
-            <button className="login_btn guest" >Sign in as Guest</button>
+            <button className="login_btn google" onClick={signInGoogle}>Sign in with Google</button>
+            <button className="login_btn guest" onClick={signInGuest} >Sign in as Guest</button>
             </div>
         </div>
     </section>
