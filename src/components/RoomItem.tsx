d2c87d4a8bf7fe6f94030deaef00b997bdda9826
centek134 <centek134@wp.pl>
2022-10-20 17:50:04 +0200

    added ;

--- src/components/RoomItem.tsx
+++ src/components/RoomItem.tsx
@@ -10,5 +10,5 @@ type Props = {
 export const RoomItem = ({ roomName, roomId}:Props) => {
   return (
     <Link className="room_link" to={`/room/${roomId}`}><li className="room_item"><p className="room_text"># {roomName}</p></li></Link>
-  )
-}
+  );
+};
