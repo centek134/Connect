b6da77c4ac11ebf9572356afcb0891750e368f3a
centek134 <centek134@wp.pl>
2022-10-23 18:55:58 +0200

    added class and styled image that was sent

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -8,14 +8,14 @@ interface Props {
     imgUrl?:string
 }
 export const Message = ({message, timeStamp, userImage, userName, imgUrl}:Props) => {
+
   return (
-      
     <div className="message">
         <img alt="user" className="message_user_image" src={userImage}/>
         <div className="text_wrapp">
             <h4 className="message_username">{userName} <span className="message_timestamp">{new Date(timeStamp?.toDate()).toUTCString()}</span></h4>
             <p className="message_text">{message}</p>
-            {imgUrl? <img src={imgUrl} alt="img that was uploaded by user"/> : null}
+            {imgUrl? <img className="message_img" src={imgUrl} alt="img that was uploaded by user"/> : null}
         </div>
     </div>
   );
