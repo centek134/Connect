582372f1faa92d922d947da272dcc8dbc8dbe986
centek134 <centek134@wp.pl>
2022-10-25 19:03:41 +0200

    added zoom in and zoom out img functionality

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/Message/Message.css";
 interface Props {
     message: string,
@@ -9,13 +9,15 @@ interface Props {
 }
 export const Message = ({message, timeStamp, userImage, userName, imgUrl}:Props) => {
 
+  const [zoomImg, setZoomImg] = useState<boolean>(false);
+
   return (
     <div className="message">
         <img alt="user" className="message_user_image" src={userImage}/>
         <div className="text_wrapp">
             <h4 className="message_username">{userName} <span className="message_timestamp">{new Date(timeStamp?.toDate()).toUTCString()}</span></h4>
             <p className="message_text">{message}</p>
-            {imgUrl? <img className="message_img" src={imgUrl} alt="img that was uploaded by user"/> : null}
+            {imgUrl? <div onClick={() => setZoomImg(!zoomImg)} className={zoomImg? "container_message_img zoom" :"container_message_img" }><img className="message_img" src={imgUrl} alt="img that was uploaded by user"/></div> : null}
         </div>
     </div>
   );
