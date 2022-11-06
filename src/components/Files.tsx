6faf4106865c58e9ef0da257264f25833b7b7873
centek134 <centek134@wp.pl>
2022-11-06 13:17:41 +0100

    created template file look

--- src/components/Files.tsx
+++ src/components/Files.tsx
@@ -32,7 +32,10 @@ export const Files = () => {
         {fileFolders.map((item, i) => <button onClick={() => console.log("działam")} className="room-list__button" key={i}>{item.fileFoldersName}</button>)}
       </section>
       <section className="files-container__file-list">
-        
+        <div className="file-list__item-wrap">
+          <img alt="img posted by user" className="file-list__item-wrap__img" src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"/>
+          <p className="file-list__item-wrap__name">File name</p>
+        </div>
       </section>
     </main>
 
