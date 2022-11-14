e2f2da1fbea90ec71915c0f9a99ebdbe8d6db3db
centek134 <centek134@wp.pl>
2022-11-14 19:23:40 +0100

    commented input file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ReactComponent as SearchIcon} from "../assets/icons/Header/Search.svg";
+//import {ReactComponent as SearchIcon} from "../assets/icons/Header/Search.svg";
 import {ReactComponent as HelpIcon} from "../assets/icons/Header/Help.svg";
 import {ReactComponent as AvatarIcon} from "../assets/icons/Header/Avatar.svg";
 import "../assets/styles/Header/Header.css";
@@ -13,10 +13,10 @@ export const Header = (user:{userImg:string, name:string}) => {
         {user.userImg? <img className="header__user-wrapp__avatar-icon" src={user.userImg} alt="user"/> : <AvatarIcon className="header__user-wrapp__avatar-icon"/>}
         <p className="header__user-wrapp__avatar-name">{user.name}</p>
       </div>
-      <div className="header__input">
+      {/* <div className="header__input">
           <p>Search in Contact group...</p>
           <SearchIcon className="header__input__search-icon"/>
-      </div>
+      </div> */}
       <HelpIcon className="header__help-icon"/>
     </header>
   );
