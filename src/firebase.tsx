cd8cbde6d2a04bd1202e20471d21dcc30c247c26
centek134 <centek134@wp.pl>
2022-11-16 18:32:25 +0100

    imported and exported deletedoc

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore, collection, getDocs, addDoc, getDoc, serverTimestamp, doc, onSnapshot, query, orderBy} from "firebase/firestore"
+import {getFirestore, collection, getDocs, addDoc, getDoc, serverTimestamp, doc, onSnapshot, query, orderBy, deleteDoc} from "firebase/firestore"
 import {getStorage, ref, uploadBytes, list, getDownloadURL, listAll} from "firebase/storage"
 
 const firebaseConfig = {
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, app, collection, getDocs, doc, addDoc, getDoc, serverTimestamp, onSnapshot, query, orderBy, storage, ref, uploadBytes, list, getDownloadURL, listAll};
+export { db, app, collection, getDocs, doc, addDoc, getDoc, serverTimestamp, onSnapshot, query, orderBy, deleteDoc, storage, ref, uploadBytes, list, getDownloadURL, listAll};
